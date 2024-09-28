f1c15e24284e3186073a81c96eff53c4bd265124
Abhiram <abhiram@2164.v@gmail.com>
2024-09-28 06:19:03 +0530

    first commit

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,17 +1,49 @@
-/* GET 'home' page */
 const homelist = (req, res) => {
-    res.render('location-list', { title: 'Home' });
-    };
-    /* GET 'Location info' page */
-    const locationInfo = (req, res) => {
-    res.render('locations-info', { title: 'Location info' });
-    };
-    /* GET 'Add review' page */
-    const addReview = (req, res) => {
-    res.render('location-review-form', { title: 'Add review' });
-    };
-    module.exports = {
-        homelist,
-        locationInfo,
-        addReview
-        };
+  res.render('locations-list', {
+    title: 'SabuSpec Automotive Customization',
+    pageHeader: {
+      title: 'SabuSpec',
+      strapline: 'Redefining Automotive Personalization'
+    },
+    locations: [
+      {
+        name: 'Number Plates',
+        imageUrl: '/stylesheets/plate.jpg'
+      },
+      {
+        name: 'Rims',
+        imageUrl: '/stylesheets/rims.jpg'
+      },
+      {
+        name: 'PPF',
+        imageUrl: '/stylesheets/ppf.jpg'
+      },
+      {
+        name: 'Exhaust Systems',
+        imageUrl: '/stylesheets/exhaust.jpg'
+      }
+    ]
+  });
+};
+
+/* GET 'Location info' page */
+const locationInfo = (req, res) => {
+  res.render('location-info', { title: 'Location Info' });
+};
+
+/* GET 'Add review' page */
+const addReview = (req, res) => {
+  res.render('location-review-form', { title: 'Add Review' });
+};
+
+/* GET 'Contact' page */
+const contactInfo = (req, res) => {
+  res.render('contact', { title: 'Contact Us' });
+};
+
+module.exports = {
+  homelist,
+  locationInfo,
+  addReview,
+  contactInfo // Corrected here to maintain naming consistency
+};
--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,12 +1,25 @@
 const express = require('express');
 const router = express.Router();
-const ctrlLocations =
-require('../controllers/locations');
+const ctrlLocations = require('../controllers/locations');
 const ctrlOthers = require('../controllers/others');
+
 /* Locations pages */
 router.get('/', ctrlLocations.homelist);
 router.get('/location', ctrlLocations.locationInfo);
-router.get('/location/review/new', ctrlLocations.addReview);
+
+/* Review page */
+router.get('/review', (req, res) => {
+    res.render('location-review-form', { title: 'Add Review' });
+});
+
+/* Contact page */
+router.get('/contact', (req, res) => {
+    res.render('contact', { title: 'Contact Us' });
+});
+
 /* Other pages */
 router.get('/about', ctrlOthers.about);
+
 module.exports = router;
+    
+ 
